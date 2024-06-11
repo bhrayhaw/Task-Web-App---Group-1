60e59a4887cb2435d6b3d08ad6483a79a90c9027
Elijah Apreko <elijahapreko@gmail.com>
2024-06-11 17:21:07 +0000

    update the taskManager file

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,61 +1,14 @@
-// class TaskManager {
-//   constructor(currentId = 0) {
-//     this.tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-//     this.currentId = this.tasks.length
-//       ? Math.max(...this.tasks.map((task) => task.id)) + 1
-//       : currentId;
-//   }
-
-//   addTask(name, description, assignedTo, dueDate, status = "TODO") {
-//     this.tasks.push({
-//       id: this.currentId++,
-//       name,
-//       description,
-//       assignedTo,
-//       dueDate,
-//       status,
-//     });
-//     this.save();
-//   }
-
-//   deleteTask(taskId) {
-//     this.tasks = this.tasks.filter((task) => task.id !== taskId);
-//     this.save();
-//   }
-
-//   updateTaskStatus(taskId, newStatus) {
-//     const task = this.tasks.find((task) => task.id === taskId);
-//     if (task) {
-//       task.status = newStatus;
-//       this.save();
-//     }
-//   }
-
-//   updateTask(taskId, updatedTask) {
-//     const task = this.tasks.find((task) => task.id === taskId);
-//     if (task != -1) {
-//       this.tasks[task] = updatedTask;
-//       this.save();
-//     }
-//   }
-
-//   getTasks() {
-//     return this.tasks;
-//   }
-
-//   save() {
-//     localStorage.setItem("tasks", JSON.stringify(this.tasks));
-//   }
-// }
-
 class TaskManager {
-  constructor() {
+  constructor(currentId = 0) {
     this.tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    this.currentId = this.tasks.length
+      ? Math.max(...this.tasks.map((task) => task.id)) + 1
+      : currentId;
   }
 
   addTask(name, description, assignedTo, dueDate, status = "TODO") {
     const task = {
-      id: this.generateUniqueId(),
+      id: this.currentId++,
       name,
       description,
       assignedTo,
@@ -64,7 +17,7 @@ class TaskManager {
     };
     this.tasks.push(task);
     this.save();
-    return task; // Return the newly added task
+    return task;
   }
 
   deleteTask(taskId) {
@@ -83,15 +36,9 @@ class TaskManager {
   updateTask(taskId, updatedTask) {
     const existingTask = this.tasks.find((task) => task.id === taskId);
     if (existingTask) {
-      Object.assign(existingTask, updatedTask); // Update existing task
-    } else {
-      this.tasks.push(updatedTask); // Add new task
+      Object.assign(existingTask, updatedTask);
+      this.save();
     }
-    this.save();
-  }
-
-  generateUniqueId() {
-    return Date.now() + Math.random().toString(36).substr(2, 9);
   }
 
   getTasks() {
